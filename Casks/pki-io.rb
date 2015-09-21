89d0d04dc114a5793d970f1f2ee3bb19c41e88ae
Joshua Nicholson <jnicholson@vmware.com>
2015-09-21 12:03:38 -0400

    Upgrade pki.io to 0.1.2-release1.

--- Casks/pki-io.rb
+++ Casks/pki-io.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'pki-io' do
-  version '0.1.0-release1'
-  sha256 '7daf9349250c5da5f8bc1a126d704faf161977f82befb9f707bc1d788efbace4'
+  version '0.1.2-release1'
+  sha256 '75ddbfc696cbd36fc178f6442473560797999113ab9fa448e5ab8f94357a4a92'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/pki-io/admin/releases/download/#{version}/pki.io_#{version}_darwin_amd64.tar.gz"
